6fd42c986bf925005e6ccd9d660db478b759a556
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-14 11:25:16 +0200

    chore: Update NotionRoot component to use environment variable for rootPageID

--- src/app/(blog)/NotionRoot.tsx
+++ src/app/(blog)/NotionRoot.tsx
@@ -2,6 +2,7 @@ import { notion } from "@/lib/notion";
 import { ExtendedRecordMap } from "notion-types";
 
 const rootPageID = process.env.NOTION_BLOG_DB!;
+const secret = process.env.NOTION_SECRET!;
 
 async function getData(rootPageID: string): Promise<ExtendedRecordMap> {
   return await notion.getPage(rootPageID);
@@ -9,7 +10,20 @@ async function getData(rootPageID: string): Promise<ExtendedRecordMap> {
 
 export default async function NotionRoot() {
   const data: ExtendedRecordMap = await getData(rootPageID);
-  console.log("notion page data", data);
+  // console.log("notion page data", data);
+
+  fetch(`https://api.notion.com/v1/databases/${rootPageID}/query`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${secret}`,
+      "Notion-Version": "2021-08-16",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.error("Erreur:", error);
+    });
 
   return (
     <>
